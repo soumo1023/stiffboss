ae52c6495bab2e76764735fd9ff98686a729ffa2
soumodip paul <soumo1023@gmail.com>
2016-02-13 02:14:59 +0530

    good

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.swing.JOptionPane;
+class Hello
+{
+public static void main(String args[])
+{
+	int f=JOptionPane.INFORMATION_MESSAGE;
+    JOptionPane.showMessageDialog(null,"hello world","stiffler",f);
+	System.exit(0);
+}
+}
